092f31ad034f805bae74d60b62a07d93c3e59751
closetjostle <closetjostle@gmail.com>
2020-12-26 19:42:56 +0000

    in progress: image upload

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,33 +3,98 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Image;
 
 class ImageController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, $user_id)
+    public function store(Request $request)
     {
-        $validated = $request->validate([
-            'image' => 'required|image|dimensions:min_width=100,min_height=200',
+        $request->validate([
+            'url' => 'required',
+            'image' => 'required|dimensions:min_width=200,min_height=200|mimes:jpeg,png,jpg,gif,svg|'
         ]);
-        
-        $p = new Post;
-        $p->title = $validated['title'];
-        $p->content = $validated['content'];
-        $p->user_id = $user_id;
-        $p->save();
-        foreach (Tag::get() as $tag) {
-           if (! ($request[$tag->tag] === null)) {
-              $p->tags()->attach($tag->id);   
-           }
+        $image = new Image;
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = $request->image->extension();
+            //$imageName = time() . '.' . $image->getExtension();
+            Image::make($image)->resize(350,350)->save(storage_path('/images/' . $imageName));
+            $image->url = $imageName
         }
 
-        session()->flash('message', 'Post created!');
-        return redirect()->route('homepage');
+
+        
+        $request->image->move(public_path('images'), $imageName);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
